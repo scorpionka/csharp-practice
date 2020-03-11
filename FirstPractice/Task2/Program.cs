5431252c8ce56ffda3017681a56c47e3395bff0d
Алексей Кузьмицкий <a.kuzmitsky@gmail.com>
2020-03-11 07:47:05 +0300

    commit task2

---
+++ FirstPractice/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            //int Add(object? value)
+            arrayList.Add(45); //true, box
+            arrayList.Add(true); //true, box
+            arrayList.Add("Hello"); //false
+            arrayList.Add(23.45); //true, box
+        }
+    }
+}
