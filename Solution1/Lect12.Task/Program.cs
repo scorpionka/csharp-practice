ed2e34e2f29bde933e28cd460d8f3e06717a91d9
Алексей Кузьмицкий <a.kuzmitsky@gmail.com>
2020-04-23 20:05:43 +0300

    commit task1

---
+++ Solution1/Lect12.Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lect12.Task
+{
+    class UniqueItem
+    {
+        public int idOfObject;
+        public static int id = 0;
+        public UniqueItem()
+        {
+            id++;
+            idOfObject = id;
+        }
+        public UniqueItem(int id)
+        {
+            UniqueItem.id = id;
+            idOfObject = id;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueItem item1 = new UniqueItem();
+            UniqueItem item2 = new UniqueItem();
+            UniqueItem item3 = new UniqueItem();
+            Console.WriteLine(UniqueItem.id);
+            UniqueItem item4 = new UniqueItem(6);
+            UniqueItem item5 = new UniqueItem();
+            Console.WriteLine(UniqueItem.id);
+            Console.WriteLine(item1.idOfObject);
+            Console.WriteLine(item3.idOfObject);
+            Console.WriteLine(item4.idOfObject);
+        }
+    }
+}
